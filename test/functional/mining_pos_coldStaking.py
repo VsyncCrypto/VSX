697974cd98e392dc4d646355167a845edbdc5733
ZioFabry <ziofabry@hotmail.com>
2020-06-02 12:50:17 +0200

    Rebrand Phase 11: test

--- test/functional/mining_pos_coldStaking.py
+++ test/functional/mining_pos_coldStaking.py
@@ -9,9 +9,9 @@ from time import sleep
 
 from test_framework.messages import CTransaction, CTxIn, CTxOut, COIN, COutPoint
 from test_framework.mininode import network_thread_start
-from test_framework.pivx_node import PivxTestNode
+from test_framework.cari_node import CariTestNode
 from test_framework.script import CScript, OP_CHECKSIG
-from test_framework.test_framework import PivxTestFramework
+from test_framework.test_framework import CariTestFramework
 from test_framework.util import (
     assert_equal,
     assert_greater_than,
@@ -28,7 +28,7 @@ def getDelegatedUtxos(utxos):
     return [x for x in utxos if x["scriptPubKey"][:10] == '76a97b63d1']
 
 
-class PIVX_ColdStakingTest(PivxTestFramework):
+class CARI_ColdStakingTest(CariTestFramework):
 
     def set_test_params(self):
         self.num_nodes = 3
@@ -48,7 +48,7 @@ class PIVX_ColdStakingTest(PivxTestFramework):
         # Setup the p2p connections and start up the network thread.
         self.test_nodes = []
         for i in range(self.num_nodes):
-            self.test_nodes.append(PivxTestNode())
+            self.test_nodes.append(CariTestNode())
             self.test_nodes[i].peer_connect('127.0.0.1', p2p_port(i))
 
         network_thread_start()  # Start up network handling in another thread
@@ -304,7 +304,7 @@ class PIVX_ColdStakingTest(PivxTestFramework):
         assert_greater_than(len(stakeInputs), 0)
         # Create the block
         new_block = self.stake_next_block(1, stakeInputs, self.mocktime, staker_privkey)
-        # Add output (dummy key address) to coinstake (taking 100 PIV from the pot)
+        # Add output (dummy key address) to coinstake (taking 100 CARI from the pot)
         self.add_output_to_coinstake(new_block, 100)
         self.log.info("New block created (rawtx) by cold-staking. Trying to submit...")
         # Try to submit the block
@@ -445,4 +445,4 @@ class PIVX_ColdStakingTest(PivxTestFramework):
 
 
 if __name__ == '__main__':
-    PIVX_ColdStakingTest().main()
+    CARI_ColdStakingTest().main()
--- test/functional/rpc_budget.py
+++ test/functional/rpc_budget.py
@@ -4,11 +4,11 @@
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 """Test RPC commands for budget proposal creation, submission, and verification."""
 
-from test_framework.test_framework import PivxTestFramework
+from test_framework.test_framework import CariTestFramework
 from test_framework.util import *
 
 
-class BudgetProposalTest(PivxTestFramework):
+class BudgetProposalTest(CariTestFramework):
     def set_test_params(self):
         self.num_nodes = 1
 
@@ -42,12 +42,12 @@ class BudgetProposalTest(PivxTestFramework):
         assert_raises_rpc_error(-8, "Invalid block start", self.nodes[0].preparebudget,
                                 name, scheme + url, numcycles, nextsuperblock - budgetcycleblocks, address, cycleamount)
 
-        self.log.info("Test with invalid PIVX address")
-        assert_raises_rpc_error(-5, "Invalid PIVX address", self.nodes[0].preparebudget,
+        self.log.info("Test with invalid CARI address")
+        assert_raises_rpc_error(-5, "Invalid CARI address", self.nodes[0].preparebudget,
                                 name, scheme + url, numcycles, nextsuperblock, "DBREvBPNQguwuC4YMoCG5FoH1sA2YntvZm", cycleamount)
 
         self.log.info("Test with too low amount")
-        assert_raises_rpc_error(-8, "Invalid amount - Payment of 9.00 is less than minimum 10 PIV allowed", self.nodes[0].preparebudget,
+        assert_raises_rpc_error(-8, "Invalid amount - Payment of 9.00 is less than minimum 10 CARI allowed", self.nodes[0].preparebudget,
                                 name, scheme + url, numcycles, nextsuperblock, address, 9)
 
         self.log.info("Test with too high amount")
