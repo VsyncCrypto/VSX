02dbda07fb1e86bb69b67fbf238a8f9ef5419a91
VsyncCrypto <admin@vsync.io>
2020-10-26 01:02:35 -0400

    update protocol version

--- src/version.h
+++ src/version.h
@@ -12,7 +12,7 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70900;
+static const int PROTOCOL_VERSION = 70760;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
@@ -21,15 +21,15 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT  = 70900;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_1 = 70910;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_2 = 70920;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_3 = 70930;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_4 = 70940;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_5 = 70950;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT  = 70760;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_1 = 70760;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_2 = 70760;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_3 = 70760;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_4 = 70760;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT_5 = 70760;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
-static const int MIN_PEER_MNANNOUNCE = 70913;
+static const int MIN_PEER_MNANNOUNCE = 70760;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
